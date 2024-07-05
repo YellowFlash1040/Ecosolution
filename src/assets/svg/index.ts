4ef51837890fb0fd81a9f8dfcfa0fe01ba6ab6fa
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-05 14:53:26 +0200

    Update assets
    I added all of the svg files from the page mockup

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,3 +1,29 @@
 import LogoIcon from './logo.svg?react';
+import ArrowIcon from './arrow.svg?react';
+import BurgerMenuIcon from './burger-menu.svg?react';
+import CpuChargeIcon from './cpu-charge.svg?react';
+import CrossIcon from './cross.svg?react';
+import EmailIcon from './email.svg?react';
+import FacebookIcon from './facebook.svg?react';
+import GlobalEditIcon from './global-edit.svg?react';
+import InstagramIcon from './instagram.svg?react';
+import MapIcon from './map.svg?react';
+import MaximizeCircleIcon from './maximize-circle.svg?react';
+import PhoneIcon from './phone.svg?react';
+import RankingIcon from './ranking.svg?react';
 
-export { LogoIcon };
+export {
+  LogoIcon,
+  ArrowIcon,
+  BurgerMenuIcon,
+  CpuChargeIcon,
+  CrossIcon,
+  EmailIcon,
+  FacebookIcon,
+  GlobalEditIcon,
+  InstagramIcon,
+  MapIcon,
+  MaximizeCircleIcon,
+  PhoneIcon,
+  RankingIcon
+};
