8c979ddfca1c61ea32977c645023933912f5dd01
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-05 13:47:11 +0200

    Add HomePage and update rounting in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
 
 import { Layout } from './components';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+      </Route>
       <Route path="*" element={<Navigate to={'/'} />}></Route>
     </Routes>
   );
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import s from './HomePage.module.css';
+
+const HomePage = () => {
+  return <></>;
+};
+
+export default HomePage;
