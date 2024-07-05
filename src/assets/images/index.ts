459ab4f6f8936510b9ee2c9173f106fccccc8e12
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-05 22:46:05 +0200

    Update assets
    I added images folder and added an image from the main section in three versions - mobile, tablet, desktop. And added the corresponding imports

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import wind_generators_big__Mobile_ImagePath from './wind-generators-big-mobile.png';
+import wind_generators_big__Tablet_ImagePath from './wind-generators-big-tablet.png';
+import wind_generators_big__Desktop_ImagePath from './wind-generators-big-desktop.png';
+
+export {
+  wind_generators_big__Mobile_ImagePath,
+  wind_generators_big__Tablet_ImagePath,
+  wind_generators_big__Desktop_ImagePath
+};
