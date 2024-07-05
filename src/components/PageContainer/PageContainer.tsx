da612150dcba703e36dcdd05f74bb6ab818e1e51
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-05 15:49:38 +0200

    Update PageContainer
    - I made an update for the padding property
    - I added "className" property and clsx for more styling flexibility

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,11 +1,14 @@
+import clsx from 'clsx';
+
 import s from './PageContainer.module.css';
 
 interface PageContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const PageContainer = ({ children }: PageContainerProps) => {
-  return <div className={s.container}>{children}</div>;
+const PageContainer = ({ className, children }: PageContainerProps) => {
+  return <div className={clsx(className, s.container)}>{children}</div>;
 };
 
 export default PageContainer;
