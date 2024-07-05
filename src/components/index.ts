6f289274e15ac4de59d2f4ed799a842b68fcc175
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-05 15:52:00 +0200

    Update index.ts in the components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { default as PageLoader } from './PageLoader/PageLoader';
 export { default as Layout } from './Layout/Layout';
 export { default as PageContainer } from './PageContainer/PageContainer';
+export { default as Header } from './Header/Header';
+export { default as Logo } from './Logo/Logo';
+export { default as BurgerMenuButton } from './BurgerMenuButton/BurgerMenuButton';
+export { default as GetInTouchButton } from './GetInTouchButton/GetInTouchButton';
