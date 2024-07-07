923485cba8a7bb3961e6d41e0a2894c91763ad26
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-07 21:41:39 +0200

    Update BurgerMenuButton
    I added onClick prop

--- src/components/BurgerMenuButton/BurgerMenuButton.tsx
+++ src/components/BurgerMenuButton/BurgerMenuButton.tsx
@@ -4,13 +4,22 @@ import s from './BurgerMenuButton.module.css';
 import { BurgerMenuIcon } from '../../assets';
 
 interface BurgerMenuButtonProps {
-  className?: string;
-  customClassName?: string;
+  className?: string | undefined;
+  customClassName?: string | undefined;
+  onClick?: () => void;
 }
 
-const BurgerMenuButton = ({ customClassName, className }: BurgerMenuButtonProps) => {
+const BurgerMenuButton = ({
+  customClassName,
+  className,
+  onClick
+}: BurgerMenuButtonProps) => {
   return (
-    <button className={customClassName ?? clsx(s.button, className)} type="button">
+    <button
+      className={customClassName ?? clsx(s.button, className)}
+      type="button"
+      onClick={onClick}
+    >
       <BurgerMenuIcon width={16} height={16} />
     </button>
   );
