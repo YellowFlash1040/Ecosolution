775adfccd8b542183a48d0c2d5f3dfbc9ccf4cd5
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-07 21:39:26 +0200

    Update GetInTouchButton component
    I recreated this component so that now it is what it has to be

--- src/components/GetInTouchButton/GetInTouchButton.tsx
+++ src/components/GetInTouchButton/GetInTouchButton.tsx
@@ -1,24 +1,21 @@
 import clsx from 'clsx';
 
+import { ArrowIcon } from '../../assets';
+
 import s from './GetInTouchButton.module.css';
 
 interface GetInTouchButtonProps {
-  customClassName?: string;
+  customClassName?: string | undefined;
   className?: string;
 }
 
 const GetInTouchButton = ({ customClassName, className }: GetInTouchButtonProps) => {
-  const handleOnClick = () => {
-    document.getElementById('contact-us')!.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
-    <a
-      className={customClassName ?? clsx(className, s.button)}
-      href="#contact-us"
-      onClick={handleOnClick}
-    >
+    <a className={customClassName ?? clsx(className, s.button)} href="#contact-us">
       Get in touch
+      <div className={s.arrowIcon}>
+        <ArrowIcon width={8} height={9} />
+      </div>
     </a>
   );
 };
