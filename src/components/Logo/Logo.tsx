8da35340dd2b583cfd2e29266d96f5f7d3de641f
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-07-07 21:33:39 +0200

    Update Logo component
    I added a className to the logo link with font-size: 0 style, to make it the same height as the svg itself

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
 import { LogoIcon } from '../../assets';
+
+import s from './Logo.module.css';
 
 interface LogoProps {
   className?: string;
@@ -8,7 +11,7 @@ interface LogoProps {
 
 const Logo = ({ className }: LogoProps) => {
   return (
-    <Link className={className} to={'/'}>
+    <Link className={clsx(className, s.logo)} to={'/'}>
       <LogoIcon width={269} height={40}></LogoIcon>
     </Link>
   );
